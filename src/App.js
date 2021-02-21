bb04d715b2e6e24b6b9c844bad0f60284fd25bdf
Sofia-Zammitto-Benasayag <sofieblauriez@gmail.com>
2021-02-21 15:09:30 -0500

    Updated HTML & CSS

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
-import './App.css';
-import Header from "./Header";
-import Form from "./Form";
+import App from "./App.css"
+import Weather from "./Weather"
 
 
 export default function WeatherApp() {
   return (
     <div className="App">
       <div class="container">
-        <div class="row">
-          <div>
-            <Header />
-          </div>
-          <br />
-          <Form />
-          <br />
-          <div class="row weekly-weather" id="forecast"></div>
-        </div>
-      </div>
+        <Weather />    
       <footer>Check out the <a href="https://github.com/Sofia-Zammitto-Benasayag/react-weather-app">GitHub repository.</a></footer>
+      </div>
     </div>
   );
 }
 
 
-//export default App;
-
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <h1>
-          Hello From React
-        </h1>
-        <Weather city="Italy" />
-      </header>
-    </div>
-  );
-}*/
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,52 @@
+import React from "react";
+import Weather from "./Weather.css"
+
+export default function WeatherApp(){
+ return(
+        <div className="Weather">
+            <form>
+                <div className="row">
+                <div className="col-9">
+                <input type="search" placeholder="Hey, what's the weather?" className="form-control" autoFocus="on"></input>
+                </div>
+                <div className="col-3">
+                <input type="submit" value="Search" className="btn btn-primary w-100"/>
+                </div>
+                </div>
+            </form>
+         <h1>California</h1>
+         <ul>
+             <li>Sunday 12:50</li>
+             <li>Sunny</li>
+         </ul>
+         <div className="row mt-3">
+         <div className="col-6">
+         <div className="clearfix">
+         <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunnny" className="float-left"/>
+         <div className="float-left">
+         <span className="temperature">65</span>
+         <span className="unit">°C</span>
+         </div>
+         </div>
+         </div>
+         <div className="col-6">
+             <ul>
+                 <li>Precipitation: 0%</li>
+                 <li>Humidity: 19%</li>
+                 <li>Wind: 0 mph</li>
+             </ul>
+         </div>
+         </div>
+        </div>
+    );
+}
+
+/* 
+
 import axios from "axios";
 import React from "react";
 import Loader from "react-loader-spinner";
+import Weather from "./Weather.css"
 
 export default function Weather(props){
 
@@ -14,12 +60,30 @@ export default function Weather(props){
     axios.get(apiUrl).then(handleResponse);
 
     return(
+        <div className="Weather">
+         Weather
+         <h1>California</h1>
+         <ul>
+             <li>Sunday 12:50</li>
+             <li>Sunny</li>
+         </ul>
         <Loader
         type="Circles"
         color="#00BFFF"
         height={100}
         width={100}
         timeout={3000} //3 secs
-      />
+        />
+        </div>
     );
 }
+
+
+
+
+
+
+
+
+
+*/
